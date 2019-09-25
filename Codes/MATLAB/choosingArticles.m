23e30f8acc829109f8b0a68841fa53c67966dc0e
Said Fahri Altındiş <30952914+saidaltindis@users.noreply.github.com>
2019-09-25 12:03:26 +0300

    Delete choosingArticles.m

--- Codes/MATLAB/choosingArticles.m
+++
@@ -1,4 +0,0 @@
-articles = load('/home/saidaltindis/ALL FILES/PROJECTS/MULTI-LABEL CLASSIFICATION/text_article_bow.mat');
-narrowArticles = load('/home/saidaltindis/ALL FILES/PROJECTS/MULTI-LABEL CLASSIFICATION/Data/articleIndexesForEachCategory.mat');
-
-narrowArticles = narrowAr
